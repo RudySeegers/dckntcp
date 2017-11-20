c1ce6344ef203addcbdcde61404d1f4f48665c05
AbeIgnocious <k.l.kubat@gmail.com>
2017-11-21 00:45:40 +0100

    moved 96% payout transfer from hardcoded addresses to database

--- ark_delegate_manager/admin.py
+++ ark_delegate_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import EarlyAdopterExceptions, DutchDelegateStatus
+
+admin.site.register(EarlyAdopterExceptions)
+admin.site.register(DutchDelegateStatus)
--- ark_delegate_manager/models.py
+++ ark_delegate_manager/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-
-class CronTest(models.Model):
-    test_result = models.FloatField(default=0)
-
-
 class VotePool(models.Model):
     address = RegexValidator(r'A[0-9a-zA-Z]{33}$', 'Only valid address formats are allowed.')
 
