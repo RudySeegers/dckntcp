9c08877089c01630175a13d7a0b2792332bffab8
AbeIgnocious <k.l.kubat@gmail.com>
2017-11-22 14:45:31 +0100

    Added __str__ method to some models

--- ark_delegate_manager/models.py
+++ ark_delegate_manager/models.py
@@ -6,17 +6,26 @@ class Setting(models.Model):
     id = models.CharField(default='main', primary_key=True, max_length=10)
     vendorfield = models.CharField(max_length=64, default='Thank you for voting.')
 
+    def __str__(self):
+        return self.id
+
 
 class BlacklistedAddress(models.Model):
     address = RegexValidator(r'A[0-9a-zA-Z]{33}$', 'Only valid address formats are allowed.')
     ark_address = models.CharField(max_length=34, blank=True, default='', validators=[address], unique=True)
     info = models.CharField(max_length=10000, blank=True, default='')
 
+    def __str__(self):
+        return self.ark_address
+
 
 class VotePool(models.Model):
     address = RegexValidator(r'A[0-9a-zA-Z]{33}$', 'Only valid address formats are allowed.')
     ark_address = models.CharField(max_length=34, blank=True, default='', validators=[address], unique=True)
     payout_amount = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.ark_address
 
 
 class DutchDelegateStatus(models.Model):
@@ -34,6 +43,9 @@ class EarlyAdopterAddressException(models.Model):
     new_ark_address = models.CharField(max_length=34, blank=True, default='', validators=[address], unique=True)
     email = models.EmailField()
     info = models.CharField(max_length=10000, blank=True, default='')
+
+    def __str__(self):
+        return self.new_ark_address
 
 
 class ArkDelegates(models.Model):
