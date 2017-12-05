0d8647d4fdf4f40cdccb0b4009bbbd21c2b1e88a
AbeIgnocious <k.l.kubat@gmail.com>
2017-12-05 16:13:33 +0100

    refactored lock test

--- ark_delegate_manager/tests.py
+++ ark_delegate_manager/tests.py
@@ -1,16 +1,16 @@
-from ark_delegate_manager.payout_functions import set_lock_payment_run, release_lock_payment_run
+from ark_delegate_manager.custom_functions import set_lock, release_lock
 import _thread
 
 
-release_lock_payment_run(name='testlock')
+release_lock(name='testlock')
 
 def test_race():
     try:
-        set_lock_payment_run(name='testlock')
+        set_lock(name='testlock')
     except Exception as e:
         print('failed to set lock: {}'.format(e))
 
 
 for i in range(5):
         _thread.start_new_thread(test_race, ())
-release_lock_payment_run(name='testlock')
+release_lock(name='testlock')
