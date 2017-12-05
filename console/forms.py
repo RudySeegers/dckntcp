7865c89bbbefac4e00b80f24a05d5acfc2bb2504
AbeIgnocious <k.l.kubat@gmail.com>
2017-12-05 16:14:02 +0100

    initial commit

--- console/forms.py
+++ console/forms.py
@@ -1,11 +1,17 @@
 from django import forms
-from . models import UserProfile
+from console.models import UserProfile
+from django.core.validators import RegexValidator
 
 
-class UserForm(forms.ModelForm):
+class SettingsForm(forms.ModelForm):
+    EMAIL_CHOICES = (
+        (1, "Don't inform me by email about payouts and current news."),
+        (2, 'Feel free to send me emails  .'))
+    address = RegexValidator(r'A[0-9a-zA-Z]{33}$', 'Only valid address formats are allowed.')
+    main_ark_wallet = forms.CharField(validators=[address])
+
     class Meta:
         model = UserProfile
-        fields = ('main_ark_wallet',
-                  )
-        labels = {'main_ark_wallet': 'Main Ark wallet'}
+        fields = ['preferred_day', 'inform_by_email', 'main_ark_wallet',
+                  'main_ark_tag', 'payout_frequency']
 
