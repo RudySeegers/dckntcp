97294abd47ee1aa3dfe0276ec6cae282b66e7780
AbeIgnocious <k.l.kubat@gmail.com>
2017-12-05 16:14:23 +0100

    added fields related to sending emails to users

--- console/models.py
+++ console/models.py
@@ -9,9 +9,16 @@ import console.custom_model_fields as custom
 
 
 class UserProfile(models.Model):
-    SENDER_CHOICES = ((1, 'Send payout second address'), (2, 'Send payout to voting address'))
-    EMAIL_CHOICES = ((1, "Don't inform me by email about payouts and current news"), (2, 'Feel free to send me emails.'))
+    SENDER_CHOICES = (
+        (1, 'Send payout second address.'),
+        (2, 'Send payout to voting address.')
+    )
+    EMAIL_CHOICES = (
+        (1, "Don't inform me by email about payouts and current news."),
+        (2, 'Feel free to send me emails.')
+    )
     address = RegexValidator(r'A[0-9a-zA-Z]{33}$', 'Only valid address formats are allowed.')
+
     user = AutoOneToOneField(User, related_name='user', primary_key=True)
     main_ark_wallet = models.CharField(max_length=34, blank=True, default='', validators=[address])
     main_ark_tag = models.CharField(max_length=34, blank=True, null=True)
@@ -24,7 +31,13 @@ class UserProfile(models.Model):
 
     payout_frequency = models.IntegerField(choices=PAYOUT_CHOICES, default=2)
     preferred_day = custom.DayOfTheWeekField(default='8')
-    inform_by_email = models.IntegerField(default=1)
+
+    """inform by email is decided by the user in the settings tab."""
+    inform_by_email = models.IntegerField(default=1, choices=EMAIL_CHOICES)
+
+    """ send_email_about_payout is set to True during a payout run if a payout is transmitted"""
+    send_email_about_payout = models.BooleanField(default=False)
+
     def __str__(self):
         return self.main_ark_wallet
 
