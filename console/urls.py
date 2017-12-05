f5697713a06b953d32d941ab3f6edbde9561b2d4
AbeIgnocious <k.l.kubat@gmail.com>
2017-12-05 16:15:22 +0100

    added urls for saving forms

--- console/urls.py
+++ console/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^update/$', views.edit_user, name='console_update'),
+    url(r'^update/$', views.render_settings, name='console_update'),
     url(r'^$', views.console_node, name='console_node'),
     url(r'^update/saved/$', views.saved, name='saved'),
     url(r'^update/not_saved/$', views.not_saved, name='not_saved'),
@@ -10,5 +10,7 @@ urlpatterns = [
     url(r'^balancereport/@(?P<ark_address>[\w.@+-]+)$', views.balance_report, name='console_balance_report'),
 
     url(r'^arkdelegatereport/$', views.delegate_report, name='console_ark_delegate_report'),
+    url(r'^save_general_settings/$', views.save_settings, name='save_general'),
+    url(r'^save_account_settings/$', views.save_account_settings, name='save_account_settings'),
 
 ]
