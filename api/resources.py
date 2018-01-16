d7b2f710dbbee926e8a7bc9c7e16f9c99269eaea
AbeIgnocious <k.l.kubat@gmail.com>
2018-01-16 19:02:09 +0100

    added relay_nodes to /api/ resources

--- api/resources.py
+++ api/resources.py
@@ -1,8 +1,15 @@
 from tastypie.resources import ModelResource
 from console.models import UserProfile
+from relay_manager.models import RelayNodes
 
 
 class UserResource(ModelResource):
     class Meta:
         queryset = UserProfile.objects.all()
         resource_name = 'user'
+
+
+class RelayNodeResource(ModelResource):
+    class Meta:
+        queryset = RelayNodes.objects.all()
+        resource_name = 'relay_nodes'
